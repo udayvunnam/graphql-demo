53450c421c596ee7a632613a8414ac9f14fa08eb
Christian Nwamba <nwambachristian@gmail.com>
2020-03-22 18:17:07 +0400

    Exteact initial data to a new file

--- src/App.js
+++ src/App.js
@@ -1,63 +1,8 @@
 import React from 'react';
-import Flip from './components/Flip';
-import './styles.css';
 
-const initialData = [
-  {
-    type: 'Mammal',
-    img:
-      'https://res.cloudinary.com/codebeast/image/upload/c_fill,h_400,q_50,w_400/v1584867791/Animal%20Classes/view-of-elephant-in-water-247431.jpg',
-    animals: [
-      {
-        name: 'Dog'
-      },
-      {
-        name: 'Horse'
-      },
-      {
-        name: 'Antelope'
-      },
-      {
-        name: 'Cat'
-      },
-      {
-        name: 'Tiger'
-      },
-      {
-        name: 'Zebra'
-      },
-      {
-        name: 'Lion'
-      },
-      {
-        name: 'Panter'
-      }
-    ]
-  },
-  {
-    type: 'Bird',
-    img:
-      'https://res.cloudinary.com/codebeast/image/upload/c_fill,h_400,q_50,w_400/v1584867879/Animal%20Classes/shallow-focus-photography-of-green-yellow-and-blue-bird-2190209.jpg',
-    animals: [
-      {
-        name: 'Chicken'
-      },
-      {
-        name: 'Duck'
-      }
-    ]
-  },
-  {
-    type: 'Reptile',
-    img:
-      'https://res.cloudinary.com/codebeast/image/upload/c_fill,h_400,q_50,w_400/v1584868136/Animal%20Classes/photo-of-a-snake-3280908.jpg',
-    animals: [
-      {
-        name: 'Snake'
-      }
-    ]
-  }
-];
+import Flip from './components/Flip';
+import initialData from './initialData';
+import './styles.css';
 
 export default function App() {
   const [flipped, flip] = React.useState('');
@@ -78,9 +23,7 @@ export default function App() {
                   height: '100%'
                 }}
               >
-                <h2 className="title">
-                  {classData.type}
-                </h2>
+                <h2 className="title">{classData.type}</h2>
               </div>
             )}
             Back={() => (
---
+++ src/initialData.js
@@ -0,0 +1,56 @@
+export default [
+  {
+    type: 'Mammal',
+    img:
+      'https://res.cloudinary.com/codebeast/image/upload/c_fill,h_400,q_50,w_400/v1584867791/Animal%20Classes/view-of-elephant-in-water-247431.jpg',
+    animals: [
+      {
+        name: 'Dog'
+      },
+      {
+        name: 'Horse'
+      },
+      {
+        name: 'Antelope'
+      },
+      {
+        name: 'Cat'
+      },
+      {
+        name: 'Tiger'
+      },
+      {
+        name: 'Zebra'
+      },
+      {
+        name: 'Lion'
+      },
+      {
+        name: 'Panter'
+      }
+    ]
+  },
+  {
+    type: 'Bird',
+    img:
+      'https://res.cloudinary.com/codebeast/image/upload/c_fill,h_400,q_50,w_400/v1584867879/Animal%20Classes/shallow-focus-photography-of-green-yellow-and-blue-bird-2190209.jpg',
+    animals: [
+      {
+        name: 'Chicken'
+      },
+      {
+        name: 'Duck'
+      }
+    ]
+  },
+  {
+    type: 'Reptile',
+    img:
+      'https://res.cloudinary.com/codebeast/image/upload/c_fill,h_400,q_50,w_400/v1584868136/Animal%20Classes/photo-of-a-snake-3280908.jpg',
+    animals: [
+      {
+        name: 'Snake'
+      }
+    ]
+  }
+];
